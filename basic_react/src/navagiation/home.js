0eab64af72ac76d8c8c03aaf12f7e1fbf58ec5bd
pavanreddy565 <pavanreddy5658@gmail.com>
2024-03-23 11:58:28 +0530

    react navagition bar using routes is added

---
+++ basic_react/src/navagiation/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+export class Home extends Component {
+    constructor(props){
+        super(props);
+
+    }
+  render() {
+    return (
+        <h1>Welcome to the Home</h1>
+    );
+  }
+}
+
+export default Home
